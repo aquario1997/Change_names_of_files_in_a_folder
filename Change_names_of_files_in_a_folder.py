9295065ad166b0596592a0ac39386a4fc0fc57da
BalaRama Krishna Oguri <balaramakrishna.oguri@gmail.com>
2020-08-16 20:38:59 +0530

    Put the previous code into change_names function
    Although the script is going into the subdirectories,
    it is stopping when doing all the files in the first
    subdirectories and changing all the file names.
    More change needed.

--- Change_names_of_files_in_a_folder.py
+++ Change_names_of_files_in_a_folder.py
@@ -5,11 +5,17 @@ import time
 
 """Rename all the files in a folder (which does not have any subfolders) with current time with a milli second gap"""
 """First change the working directory to the required directory using os.chdir()"""
-for file in os.listdir():
-    if not os.path.isdir(file):
-        regex = r'(.*)(\.\w+)$'
-        time.sleep(0.001)
-        timenow = str(datetime.datetime.today())
-        timenow = re.sub(':', '-', timenow)
-        new_name = re.sub(regex, timenow+'\\2', file)
-        os.rename(file, new_name)
+def change_names():
+    for file in os.listdir():
+        if not os.path.isdir(file):
+            regex = r'(.*)(\.\w+)$'
+            time.sleep(0.001)
+            timenow = str(datetime.datetime.today())
+            timenow = re.sub(':', '-', timenow)
+            new_name = re.sub(regex, timenow+'\\2', file)
+            os.rename(file, new_name)
+        else:
+            os.chdir(file)
+            change_names()
+
+change_names()
