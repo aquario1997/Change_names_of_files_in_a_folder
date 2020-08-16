9b8b2074f5543b39f567ebcf90d7de4f8612b147
BalaRama Krishna Oguri <balaramakrishna.oguri@gmail.com>
2020-08-16 21:22:19 +0530

    Added the functionality to change names in subdirs
    Now, the script can change names of all the files in
    the folder it is executed in, this includes all
    subdirectories.
    
    No known issues at the moment

--- Change_names_of_files_in_a_folder.py
+++ Change_names_of_files_in_a_folder.py
@@ -5,6 +5,8 @@ import time
 
 """Rename all the files in a folder (which does not have any subfolders) with current time with a milli second gap"""
 """First change the working directory to the required directory using os.chdir()"""
+curr_dir = os.getcwd()
+
 def change_names():
     for file in os.listdir():
         if not os.path.isdir(file):
@@ -17,5 +19,8 @@ def change_names():
         else:
             os.chdir(file)
             change_names()
+    if os.getcwd() != curr_dir:
+        os.chdir('..')
+
 
 change_names()
